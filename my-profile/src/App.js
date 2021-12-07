e17d8c5b863bf03ab78390660a74988fa9defaa5
Cristiane Abreu <bjwmcd@yahoo.com>
2021-12-07 06:09:26 -0500

    update

--- my-profile/src/App.js
+++ my-profile/src/App.js
@@ -2,13 +2,16 @@ import React, {Component} from 'react';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import NavBar from './components/layout/NavBar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component{
+ 
   render(){
   return (
     <div className="App">
       <NavBar/>
+      <UserItem/>
       <Home/>
       <Contact/>
     </div>
---
+++ my-profile/src/components/users/UserItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class UserItem extends Component {
+    constructor(){
+        super();
+        this.state ={
+          id:'id',
+          login:'mojombo',
+          avatar_url:'https://avatars0.githubusercontent.com/u/1?v=4',
+          html_url:'https://github.com/mojombo'
+        }
+    }
+    render() {
+        return (
+            <div>
+                UserIteem
+            </div>
+        )
+    }
+}
+
+export default UserItem;
