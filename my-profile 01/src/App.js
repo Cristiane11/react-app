752ab840eb6d7014a4ee243eb2a4b92f658ecf04
Cristiane Abreu <bjwmcd@yahoo.com>
2021-12-16 05:40:12 -0500

    update

--- my-profile 01/src/App.js
+++ my-profile 01/src/App.js
@@ -2,18 +2,30 @@ import React, {Component} from 'react';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import NavBar from './components/layout/NavBar';
-import UserItem from './components/users/UserItem';
+//import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component{
- 
+
+  state ={
+    users:[],
+    loading:false
+  }
+ async componentDidMount(){
+   this.setState({loading:true});
+    const res = await axios
+    .get('https://api.github.com/users');//.then(res=>console.log(res.data))
+    this.setState({users: res.data, loading:false})
+    
+  }
   render(){
   return (
     <div className="App">
       <NavBar/>
       <div className="container">
-      <Users/>
+      <Users loading={this.state.loading} users={this.state.users}/>
       <Home/>
       <Contact/> 
       </div>
